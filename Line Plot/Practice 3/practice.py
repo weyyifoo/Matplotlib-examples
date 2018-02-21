4d4c9fa9ef4f243f61e27810c05efc87cca02250
Weyyi <fwy2187@gmail.com>
2018-02-21 11:15:57 -0600

    1

--- Line Plot/Practice 3/practice.py
+++ Line Plot/Practice 3/practice.py
@@ -9,18 +9,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-def f1(n):
-    y = np.sin(n)
-    return y
+def f1(n1):
+    y1 = np.sin(n1)
+    return y1
 
-x = np.arange(0, 100, 0.1)
-y = f1(x)
+def f2(n2):
+    y2 = np.exp(n2)
+    return y2
+
+x1 = np.arange(0, 100, 0.1)
+y1 = f1(x1)
+x2 = np.arange(0, 5, 0.1)
+y2 = f2(x2)
 
 fig1 = plt.figure(1)
-plt.subplot(311)
-plt.plot(x, y, 'bo')
+plt.subplot(211)
+plt.plot(x1, y1, 'bo')
+plt.subplot(212)
+plt.plot(y1,x1,'rd')
 
 fig2 = plt.figure(2)
 plt.subplot(211)
-plt.plot(y,x,'rd')
+plt.plot(x2, y2, 'rs')
+plt.subplot(212)
+plt.plot(y2, x2, 'k-')
+
 plt.show()
